6b6be39cfa61b2b3b3e9cc8c98b135b0e72f6d61
XedonDEV <xedon@arctic-network.com>
2017-07-02 15:37:45 +0200

    v1.0.9

--- config.js
+++ config.js
@@ -18,5 +18,9 @@ var APIModHashlistURL = 'mod/hashlist/'
 
 var STATICFILESERVE = 'http://webstorage1.gaming-provider.com/dwcentral/ArtOfRP/modpack/@AORv2_ArtOfRp.de/'
 
+Array.prototype.extend = function (other_array) {
+    /* you should include a test to check whether other_array really is an array */
+    other_array.forEach(function(v) {this.push(v)}, this);
+}
 
 
